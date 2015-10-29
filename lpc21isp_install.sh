aeaa9199e523aa4674de86db455a3b5f535050a4
Donghee <i4u_4ever@yahoo.com>
2015-10-29 17:12:41 +0900

    Update lpc21isp_install.sh

--- lpc21isp_install.sh
+++ lpc21isp_install.sh
@@ -1,8 +1,16 @@
-curl -L https://github.com/capiman/lpc21isp/archive/master.zip > lpc21isp.zip
-unzip lpc21isp.zip
-cd lpc21isp-master
-make
-chmod +x lpc21isp
+if [[ "$(expr substr $(uname -s) 1 9)" == "CYGWIN_NT" ]]; then
+  curl -L https://github.com/Ladvien/LPC21ISP_Win/archive/master.zip > lpc21isp.zip
+  unzip lpc21isp.zip
+  cd LPC21ISP_WIN-master
+  chmod +x lpc21isp.exe
+else 
+  curl -L https://github.com/capiman/lpc21isp/archive/master.zip > lpc21isp.zip
+  unzip lpc21isp.zip
+  cd lpc21isp-master
+  make
+  chmod +x lpc21isp
+fi
+
 export PATH="`pwd`:$PATH"
 cd ..
 rm lpc21isp.zip
